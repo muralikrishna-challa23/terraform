041f38d6e61c920f8029c387cb0562d7093b9ca0
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-10-25 21:14:34 +0530

    terraform

---
+++ data-source/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "terraform"{
+    ami = data.aws_ami.ami_query.id
+    instance_type ="t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_all_tf.id]
+
+    tags = {
+        Name = "data"
+        terraform = true
+    }
+}
+
+
+
+
+resource "aws_security_group" "allow_all_tf" {
+  name        = "allow_all_tf"
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_all_tf"
+  }
+}
