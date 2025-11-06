524b5aa8eab85707d9146584d7e3dd0ddce622fc
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-11-06 22:12:11 +0530

    provisioners

--- provisioners/provider.tf
+++ provisioners/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
     backend "s3" {
     bucket = "devops-practice-mkreddy"
-    key    = "remote-state-demo"
+    key    = "provisioners-demo"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
