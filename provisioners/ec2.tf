d46691a35704b41b3821ed99eff01f5663ffddf8
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-11-06 21:27:48 +0530

    provisioners

---
+++ provisioners/ec2.tf
@@ -0,0 +1,43 @@
+resource "aws_instance" "terraform" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all_tf.id]
+
+  tags = {
+    Name = "terraform"
+    terraform = "true"
+  }
+
+  provisioner "local_exec" {
+    command = "echo ${self.private_id} > inventory "
+    on_failure = continue
+  }
+
+    provisioner "local_exec" {
+    command = "It destroyed "
+    when = destroy
+  }
+}
+
+
+resource "aws_security_group" "allow_all_tf" {
+  name        = "allow_all_tf"
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_all_tf"
+  }
+}
